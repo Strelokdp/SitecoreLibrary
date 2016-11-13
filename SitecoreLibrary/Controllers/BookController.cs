cd78c4fa317f0c78282fa9bd892455af5e6aa72d
Женя <laeda@list.ru>
2016-11-13 23:31:32 +0300

    Implemented post service

--- SitecoreLibrary/Controllers/BookController.cs
+++ SitecoreLibrary/Controllers/BookController.cs
@@ -15,6 +15,7 @@ namespace SitecoreLibrary.Controllers
     public class BookController : Controller
     {
         private readonly BookService _bookService = new BookService();
+        private readonly PostService _postService = new PostService();
 
         // GET: BookWithAuthor/GetAllBooks
         public ActionResult GetAllBooks(string selectList, string currentFilter, string sortOrder, int? page)
@@ -123,29 +124,8 @@ namespace SitecoreLibrary.Controllers
                 }
 
                 ViewBag.AlertMsg = "Book was taken";
-                var fromAddress = new MailAddress("sitecorelibrary2016@gmail.com", "Sitecore Library");
-                var toAddress = new MailAddress(eMail, eMail);
-                const string fromPassword = "sitecore2016";
-                const string subject = " has been taken";
-                const string body = "You have just taken book, don't forget to return it";
-
-                var smtp = new SmtpClient
-                {
-                    Host = "smtp.gmail.com",
-                    Port = 587,
-                    EnableSsl = true,
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    UseDefaultCredentials = false,
-                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-                };
-                using (var message = new MailMessage(fromAddress, toAddress)
-                {
-                    Subject = bookName + subject,
-                    Body = body
-                })
-                {
-                    smtp.Send(message);
-                }
+                
+                _postService.SendBookTakenEmail(eMail, bookName);
 
                 return RedirectToAction("GetAllBooks");
 
@@ -168,29 +148,9 @@ namespace SitecoreLibrary.Controllers
                 }
 
                 ViewBag.AlertMsg = "Book was returned";
-                var fromAddress = new MailAddress("sitecorelibrary2016@gmail.com", "Sitecore Library");
-                var toAddress = new MailAddress(eMail, eMail);
-                const string fromPassword = "sitecore2016";
-                const string subject = "Book has been returned";
-                const string body = "You have just returned book. Hope you liked it";
 
-                var smtp = new SmtpClient
-                {
-                    Host = "smtp.gmail.com",
-                    Port = 587,
-                    EnableSsl = true,
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    UseDefaultCredentials = false,
-                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-                };
-                using (var message = new MailMessage(fromAddress, toAddress)
-                {
-                    Subject = subject,
-                    Body = body
-                })
-                {
-                    smtp.Send(message);
-                }
+                _postService.SendBookReturnedEmail(eMail);
+
                 return RedirectToAction("GetAllBooks");
 
             }
